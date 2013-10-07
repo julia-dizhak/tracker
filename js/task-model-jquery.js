a850442af55aa5bc0e1e1eefe0619483ad504195
Julia Scherbina <julia.shcherbina@djangostars.com>
2013-10-07 12:32:57 +0300

    added status prog, deleted task-item

--- js/task-model-jquery.js
+++ js/task-model-jquery.js
@@ -6,7 +6,7 @@ $(function(){
          { name: 'Task1 first task name', desc: 'Description first task', status: 'todo' },
          { name: 'Task2 second task name', desc: 'Description second task', status: 'inprogress' },
          { name: 'Task3 third task name', desc: 'Description third task', status: 'done'},
-         { name: 'Task4 fourth task name', desc: 'Description fourth task', status: 'done'},
+         { name: 'Task4 fourth task name', desc: 'Description fourth task', status: 'todo'},
          { name: 'Task5', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol', status: 'done'}
     ];  
     
@@ -23,25 +23,39 @@ $(function(){
                             '<div class=\'task-layout\'>' +  
                             '<div class=\'task-name\'>' + item.name + '</div>' +
                             '<div class=\'task-desc\'>' + item.desc + '</div>' +
-                            '<div class=\'task-status\'>' + item.status + '</div>' +
+                            '<div class=\'task-status\'>' + '<p>' + item.status + '</p>' + '</div>' +
+                            '<a href="#" class=\'btn-delete\'>' + 'delete task' + '</a>' +
                             '</div>'
                             '<i class=\'cube-bottom\'></i>'+
                           '</div>' +
-                      '</div>'; 
+                      '</div>';  
+                        
       $(render).appendTo(project);
       
       return render;
-    }    
+    }   
     
-      
-
     for(var i in tasks){
       addTask(tasks[i]);
     }
-       
     
+    $('.task-status').each(function(index, elem) {
+      $(this).addClass( $(elem).text());
+    });
+   
     
+    function createTask() {
+      
+    }
     
-  })();
+    function deleteTask(item) {
+      console.log(item);
+    }
+    
+    $('.btn-delete').on('click',function(){
+      $(this).closest('.task-item').hide('slow', function(){ $(this).closest('.task-item').remove(); });
+    });
+        
+})();
   
 });
