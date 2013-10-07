eb7635de3be8a70b00170ba08ca82f920e3b1f88
Julia Scherbina <julia.shcherbina@djangostars.com>
2013-10-07 11:48:01 +0300

    addes task model, add css, add js

--- js/task-model-jquery.js
+++ js/task-model-jquery.js
@@ -1,23 +1,30 @@
 $(function(){
   
   (function(){
-    
+    var projectHeader = 'Project Header';
     var tasks = [
-         { name: 'Task1 initial name', desc: 'Description 1 task', status: 'todo' },
-         { name: 'Task2 second name', desc: 'Description second task', status: 'in-progress' },
-         { name: 'Task3', desc: 'Description third', status: 'done'}
-    ];
+         { name: 'Task1 first task name', desc: 'Description first task', status: 'todo' },
+         { name: 'Task2 second task name', desc: 'Description second task', status: 'inprogress' },
+         { name: 'Task3 third task name', desc: 'Description third task', status: 'done'},
+         { name: 'Task4 fourth task name', desc: 'Description fourth task', status: 'done'},
+         { name: 'Task5', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dol', status: 'done'}
+    ];  
     
     var project = $('<div />').appendTo('body').addClass('project'),
-        headerProject = $('<header />').appendTo(project).addClass('project-name');
+         projectHeader = $('<header />').appendTo(project);
         
+    project.wrap('<div class="wrapper"></div>'); 
+    projectHeader.text('projectHeader');   
         
-    function addTask(){
-      //var render = '<div />';
+    function addTask(item){
       var render = '<div class=\'task-item\'>' +
                           '<div class=\'cube\'>' +
                             '<i class=\'cube-top\'></i>'+
-                            '<div class=\'task-layout\' />' +
+                            '<div class=\'task-layout\'>' +  
+                            '<div class=\'task-name\'>' + item.name + '</div>' +
+                            '<div class=\'task-desc\'>' + item.desc + '</div>' +
+                            '<div class=\'task-status\'>' + item.status + '</div>' +
+                            '</div>'
                             '<i class=\'cube-bottom\'></i>'+
                           '</div>' +
                       '</div>'; 
@@ -26,11 +33,10 @@ $(function(){
       return render;
     }    
     
-    
       
 
     for(var i in tasks){
-      addTask();
+      addTask(tasks[i]);
     }
        
     
